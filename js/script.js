7117022df72bc2cb6b552bedaed24fec7e44bf46
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-04-30 11:17:06 +0900

    [Finish]

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 $(function(){
-  $('.menu-trigger').on('click', function(event){
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "7e081589859a367b15d0c02500f8b2f1",
+      dataType: "jsonp",
+    }).done(function(data) {
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('#img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('#img').attr("alt", data.weather[0].main);
+    }).fail(function(data) {
+      alert("通信に失敗しました。");
+    });
   });
 });
